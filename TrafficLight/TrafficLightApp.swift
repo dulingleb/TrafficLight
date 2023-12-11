5f66a3cc3b2b9e2588c999d7d4b8927a1df06011
dulingleb <dulingleb@gmail.com>
2023-12-11 19:57:44 +0100

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Dulin Gleb on 11.12.23..
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
